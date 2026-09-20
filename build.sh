493f6b9fc07576fa83daf0ac5e3f6bfc7804d29b
Arham Abbasi <arhamabbasi590@gmail.com>
2026-09-20 20:37:16 +0530

    Auto-create superuser and seed data on deploy

--- build.sh
+++ build.sh
@@ -3,3 +3,14 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py seed_demo_data
+
+python manage.py shell -c "
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='Admin').exists():
+    User.objects.create_superuser('Admin', 'admin@example.com', 'arham123')
+    print('Superuser created')
+else:
+    print('Superuser already exists')
+"
